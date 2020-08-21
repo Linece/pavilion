1a55742aae88431399738083f786920af0e76468
Linece <41286031+Linece@users.noreply.github.com>
2020-08-21 09:03:27 +0800

    取消数据库连接

--- src/main/java/com/pavilion/PavilionApplication.java
+++ src/main/java/com/pavilion/PavilionApplication.java
@@ -2,8 +2,9 @@ package com.pavilion;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
 public class PavilionApplication {
     public static void main(String[] args) {
         new SpringApplication(PavilionApplication.class).run(args);
