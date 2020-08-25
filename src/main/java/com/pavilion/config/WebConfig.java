8db1249439e7b47e4307ff0f7bae0bd17142f343
Linece <41286031+Linece@users.noreply.github.com>
2020-08-25 21:27:59 +0800

    前端代码

--- src/main/java/com/pavilion/config/WebConfig.java
+++ src/main/java/com/pavilion/config/WebConfig.java
@@ -8,7 +8,7 @@ import org.springframework.web.servlet.view.InternalResourceViewResolver;
  * 跨域问题解决
  */
 @Configuration
-@EnableWebMvc
+//@EnableWebMvc(注释防止静态资源不能访问)
 public class WebConfig implements WebMvcConfigurer {
 
 	@Override
@@ -20,9 +20,9 @@ public class WebConfig implements WebMvcConfigurer {
 	 * 静态资源访问配置
 	 * @param registry
 	 */
-	@Override
-	public void addResourceHandlers(ResourceHandlerRegistry registry) {
-		registry.addResourceHandler("/**")
-				.addResourceLocations("classpath:/static/");
-	}
+//	@Override
+//	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+//		registry.addResourceHandler("/**")
+//				.addResourceLocations("classpath:/static/");
+//	}
 }
