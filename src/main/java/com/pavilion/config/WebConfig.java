a0ce590da863c9bc675cf5074791e07dc16335c5
zhudecheng <862860351@qq.com>
2020-08-25 18:17:58 +0800

    代码

--- src/main/java/com/pavilion/config/WebConfig.java
+++ src/main/java/com/pavilion/config/WebConfig.java
@@ -1,9 +1,8 @@
 package com.pavilion.config;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.*;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 /**
  * 跨域问题解决
@@ -15,5 +14,15 @@ public class WebConfig implements WebMvcConfigurer {
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
 		registry.addMapping("/**");
+	}
+
+	/**
+	 * 静态资源访问配置
+	 * @param registry
+	 */
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("/**")
+				.addResourceLocations("classpath:/static/");
 	}
 }
